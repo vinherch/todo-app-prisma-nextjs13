dfc2d39213dfa9e4bbfd0263f9615c79d6be6f47
Herzog Vincenz <vincenz.herzog@merbag.ch>
2023-10-12 15:31:45 +0200

    Using API for fetching todo's

--- src/components/home/Todos.tsx
+++ src/components/home/Todos.tsx
@@ -1,6 +1,5 @@
 import ControlTodos from "./ControlTodos";
 import TodoList from "./TodoList";
-import { PrismaClient } from "@prisma/client";
 
 type TodosProps = {
   userId: number;
@@ -10,22 +9,19 @@ type TodosProps = {
 type Todos = { id: number; userId: number; title: string; description: string; checked: boolean; created: Date; updated: Date }[];
 
 const getTodos = async () => {
-  const prisma = new PrismaClient();
-  const data = await prisma.todo.findMany({
-    where: {
-      userId: 1,
-    },
+  const response = await fetch("http://localhost:3000/api/v1.0/todos", {
+    cache: "no-store",
   });
-  return data;
+  return await response.json();
 };
 
 const Todos = async ({ userId, sort }: TodosProps) => {
-  const data = await getTodos();
+  const todos = await getTodos();
   return (
     <div>
       <ControlTodos />
       <div className="mt-3 border-t border-t-gray-300 ">
-        <TodoList todos={data} sort={sort} />
+        <TodoList todos={todos} sort={sort} />
       </div>
     </div>
   );
