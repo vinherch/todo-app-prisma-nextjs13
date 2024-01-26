a191f3a2f7b87f4d4fa3b2bfedfcb51233ba3925
Vincenz Herzog <herzogv88@gmail.com>
2024-01-26 16:00:16 +0100

    feat: add alert component

---
+++ src/components/shared/Alert.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+type AlertType = "error" | "warning" | "success" | "information";
+
+type AlertProps = {
+  message: string;
+  alertType: AlertType;
+  duration?: number | 2000;
+};
+
+//TODO
+//Remove - Transition.
+//Types entweder oder - Typescript - bei Duration
+
+const Alert = ({ message, alertType, duration }: AlertProps) => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+  }, []);
+
+  return alertType == "error" && showAlert ? (
+    <div role="alert" className="alert alert-error">
+      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+      <span>{message}</span>
+    </div>
+  ) : alertType == "success" && showAlert ? (
+    <div role="alert" className="alert alert-success">
+      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+      <span>{message}</span>
+    </div>
+  ) : alertType == "warning" && showAlert ? (
+    <div role="alert" className="alert alert-warning">
+      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      <span>{message}</span>
+    </div>
+  ) : alertType == "information" && showAlert ? (
+    <div role="alert" className="alert alert-info">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span>New software update available.</span>
+    </div>
+  ) : null;
+};
+
+export default Alert;
