eaaec54a01364157c661af8a205ac1c0d561448c
Vincenz Herzog <herzogv88@gmail.com>
2024-02-06 17:41:51 +0100

    Update avatar component

--- src/components/shared/Avatar.tsx
+++ src/components/shared/Avatar.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 type AvatarProps = {
-  userData?: {};
+  userData: UserData;
+};
+
+type UserData = {
+  id: number;
+  email: string;
+  firstname: string | null;
+  lastname: string | null;
+  created: string;
+  updated: string;
 };
 
 const Avatar = ({ userData }: AvatarProps) => {
   return (
     <div className="text-neutral-content rounded-full w-10 bg-slate-500">
-      <span className="text-xs">AA</span>
+      <span className="text-xs">{userData.email.slice(0, 2).toLocaleUpperCase()}</span>
     </div>
   );
 };
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import UserMenu from "../home/UserMenu";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
