defaa55dc28f3ea145b1ab541b39cd428ea01976
Vincenz Herzog <herzogv88@gmail.com>
2024-02-15 17:36:12 +0100

    Update layout todo component

--- src/components/home/Todo.tsx
+++ src/components/home/Todo.tsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import TodoContext from "@/context/TodoContext";
 import DeleteTodoBtn from "./DeleteTodoBtn";
 import TodoTag from "./TodoTag";
+import { getDateTime } from "@/utils/dataTimeHelper";
 
 type TodoProps = {
   id: number;
@@ -22,36 +23,42 @@ const Todo = ({ id, userId, title, description, checked, created, updated }: Tod
   const [isChecked, setIsChecked] = useState(false);
   const { deleteTodo, todoTags } = useContext(TodoContext);
   return (
-    <div className="card w-full h-96 bg-neutral text-neutral-content shadow-lg shadow-gray-500 md:hover:scale-105 transition-all duration-200">
+    <div className="card w-full h-96 bg-neutral text-neutral-content shadow-lg shadow-gray-500 md:hover:scale-105 transition-all duration-20">
       <div className="card-body items-center">
         <h2 style={{ textDecoration: isChecked ? "line-through" : "" }} className="card-title">
           {title}
         </h2>
-        <p style={{ textDecoration: isChecked ? "line-through" : "" }} className="text-sm w-full overflow-auto">
-          {description}
-        </p>
-        <div className="card-actions w-full justify-end items-end">
-          <div className="flex justify-center gap-2">
-            <label htmlFor={`completed-${id}`} className="text-sm">
-              Completed
-            </label>
-            <input type="checkbox" checked={checked} id={`completed-${id}`} className="checkbox" onChange={() => setIsChecked(!isChecked)} />
-          </div>
-          <DeleteTodoBtn todoId={id} />
-        </div>
-        <div className="flex w-full">
-          <div className="flex flex-1">
-            <p className="text-sm">Assigned Tag's</p>
-            {/* {todoTags.map((e) => e.id === id && e.tags.map((t) => <TodoTag key={t} value={t} />))} */}
-          </div>
-          <div className="text-xs">
-            <div className="flex justify-end items-end gap-1">
-              <p>Created: </p>
-              <span>{created}</span>
+        <div className="flex flex-col h-full w-full gap-2 p-2">
+          <div className=" flex-1 border border-gray-600 p-1">
+            <div style={{ textDecoration: isChecked ? "line-through" : "" }} className="text-sm w-full overflow-auto ">
+              {description}
             </div>
-            <div className="flex justify-end items-end gap-1">
+          </div>
+          <div className="card-actions w-full p-1">
+            <div className="flex-1 h-full">
+              <input type="checkbox" checked={checked} id={`completed-${id}`} onChange={() => setIsChecked(!isChecked)} />
+              <label htmlFor={`completed-${id}`} className="text-xs ml-2">
+                Completed
+              </label>
+            </div>
+            <div>
+              <DeleteTodoBtn todoId={id} />
+            </div>
+          </div>
+          <div className="flex w-full p-1">
+            <div className="flex flex-1">
+              <p className="text-xs">Tags: </p>
+              {/* {todoTags.map((e) => e.id === id && e.tags.map((t) => <TodoTag key={t} value={t} />))} */}
+            </div>
+          </div>
+          <div className="text-xs text-gray-400 p-1">
+            <div className="flex gap-1">
+              <p>Created: </p>
+              <span>{getDateTime(created)}</span>
+            </div>
+            <div className="flex gap-1">
               <p>Updated: </p>
-              <span>{updated}</span>
+              <span>{getDateTime(updated)}</span>
             </div>
             {/* <div className="flex justify-end gap-1">
               <p>Due Date: </p>
