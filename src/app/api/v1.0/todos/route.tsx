3a3c69cd0bbcd51a2d0d5dc86168715a003bb775
Vincenz Herzog <herzogv88@gmail.com>
2024-02-21 16:13:18 +0100

    Update api route - todos

--- src/app/api/v1.0/todos/route.tsx
+++ src/app/api/v1.0/todos/route.tsx
@@ -2,6 +2,22 @@ import prisma from "@/db/prismaClient";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
+  //Get all todos / Get todos by user id
+  const { searchParams } = new URL(req.url);
+  const user: string | null = searchParams.get("user");
+  if (user) {
+    try {
+      const data = await prisma.todo.findMany({
+        where: {
+          userId: Number.parseFloat(user),
+        },
+      });
+      if (!data) return NextResponse.json({ msg: "Not Found" }, { status: 404 });
+      return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+      return NextResponse.json(error, { status: 500 });
+    }
+  }
   try {
     const data = await prisma.todo.findMany();
     return NextResponse.json(data, { status: 200 });
