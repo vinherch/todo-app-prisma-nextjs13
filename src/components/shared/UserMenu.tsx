c7c1e85499fee9a10490cebeaefbfa43ec6e70dc
Vincenz Herzog <herzogv88@gmail.com>
2024-02-23 19:47:19 +0100

    Refactor: User menu

--- src/components/shared/UserMenu.tsx
+++ src/components/shared/UserMenu.tsx
@@ -1,24 +1,20 @@
+import Link from "next/link";
 import Avatar from "./Avatar";
 import Logout from "../home/Logout";
-import { cookies } from "next/headers";
-import { verifyJWT } from "@/utils/jwtHelper";
-
-const getUserDetails = async () => {
-  const jwt = cookies().get("auth-token");
-  const { id } = await verifyJWT(jwt!.value);
-  const response = await fetch(`${process.env.HOST_DEV}/api/v1.0/users/${id}`);
-  return await response.json();
-};
+import { getLoggedInUser } from "@/actions/actions";
 
 const UserMenu = async () => {
-  const userData = await getUserDetails();
+  const user = await getLoggedInUser();
   return (
     <div className="flex-none gap-2 border">
       <div className="dropdown dropdown-end">
         <label tabIndex={0} className="avatar placeholder">
-          <Avatar userData={userData} />
+          <Avatar userData={user} />
         </label>
         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-60">
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
           <li>
             <Logout />
           </li>
